634ba641e19a02bd270301a644c8988de93950d5
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-04-24 11:55:41 -0600

    refactor: (models): remove choices from Prospect model structure
        Modified the structure of the Prospect model by removing choices.

--- prospects/models.py
+++ prospects/models.py
@@ -2,18 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Prospect(models.Model):
-    EDUCATION_LEVEL_CHOICES = [
-        ('High School', 'Secundaria'),
-        ('Preparatory', 'Preparatoria'),
-        ('Bachelor', 'Licenciatura')
-    ]
-
     full_name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = models.CharField(max_length=20)
-    education_level = models.CharField(max_length=50, choices=EDUCATION_LEVEL_CHOICES)
+    education_level = models.CharField(max_length=100)
     currently_employed = models.BooleanField(default=False)
     sales_experience = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.full_name

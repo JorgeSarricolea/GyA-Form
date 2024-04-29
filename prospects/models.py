c35bd23eb9446d727ff55712cebb500a286b718f
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-04-29 14:42:55 -0600

    refactor: (models): update Prospect model
        Modified the Prospect model to replace 'full name' field with 'first name', 'last name', and 'mother's last name'.

--- prospects/models.py
+++ prospects/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Prospect model
 class Prospect(models.Model):
-    full_name = models.CharField(max_length=100)
+    # foreign key of the main user table
+    # user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, verbose_name=_("Usuario"))
+
+    first_name = models.CharField(max_length=50, default='')
+    last_name = models.CharField(max_length=50, default='')
+    mother_last_name = models.CharField(max_length=50, default='')
     email = models.EmailField()
     phone_number = models.CharField(max_length=20)
     education_level = models.CharField(max_length=100)

4faaa73488155a54b030161604e410134339ca64
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-04-29 14:22:04 -0600

    feat: (form): add 'Privacy Notice' checkbox to form
        Added a 'Privacy Notice' checkbox to the form, which must be checked for the form to be submitted.

--- prospects/models.py
+++ prospects/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Prospect model
 class Prospect(models.Model):
     full_name = models.CharField(max_length=100)
     email = models.EmailField()

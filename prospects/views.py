bb102dba08b7fc6e67213fd68e7dc60dec4b1e69
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-04-29 21:55:27 -0600

    refactor: (views): separate email sending function for Recluta Team
        Split email sending function from 'create_prospect' to 'send_email_to_recluta' for better code comprehension and scalability.

--- prospects/views.py
+++ prospects/views.py
@@ -9,6 +9,30 @@ def show_form(request):
     return render(request, 'form.html')
 
 # Create a new prospect
+def send_email_to_recluta(prospect):
+    try:
+        # Create context with prospect data
+        context = {'prospect': prospect}
+
+        # Render the email template with the context
+        email_content = render_to_string('recluta_email_template.html', context)
+
+        # Create and send the email message
+        email = EmailMessage(
+            subject='¡Nuevo prospecto registrado!',
+            body=email_content,  # Pass the HTML content directly to the body
+            from_email=settings.EMAIL_HOST_USER,
+            to=[settings.EMAIL_HOST_USER],
+        )
+        email.content_subtype = 'html'  # Set content type as HTML
+        email.fail_silently = False
+
+        # Send email to Recluta Team
+        email.send()
+        print('Email sent to Recluta Team!')
+    except Exception as e:
+        print(f'Error sending email: {str(e)}')
+
 def create_prospect(request):
     if request.method == 'POST':
         try:
@@ -38,27 +62,8 @@ def create_prospect(request):
             prospect.save()
             print('New prospect successfully saved in DB!')
 
-            # Create context with prospect data
-            context = {
-                'prospect': prospect,
-            }
-
-            # Render the email template with the context
-            email_content = render_to_string('recluta_email_template.html', context)
-
-            # Create and send the email message
-            email = EmailMessage(
-                subject='¡Nuevo prospecto registrado!',
-                body=email_content,  # Pass the HTML content directly to the body
-                from_email=settings.EMAIL_HOST_USER,
-                to=[settings.EMAIL_HOST_USER],
-            )
-            email.content_subtype = 'html'  # Set content type as HTML
-            email.fail_silently = False
-
-             # Send email to Recluta Team
-            email.send()
-            print('Email sent to Recluta Team!')
+            # Send email to Recluta Team
+            send_email_to_recluta(prospect)
 
             return redirect('/prospects/')
         except Exception as e:

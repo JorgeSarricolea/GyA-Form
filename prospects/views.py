1bc0de9edfefa97541b5697da684233b92f581ea
Jorge Sarricolea <jjorgesarricolea18@email.com>
2024-05-06 12:23:02 -0600

    feat: (views): add function for sending thank you email to prospect
        Implemented new function to send a thank you email to prospects.

--- prospects/views.py
+++ prospects/views.py
@@ -17,39 +17,6 @@ last_email_index = 0
 def show_form(request):
     return render(request, 'form.html')
 
-# Send email to recluta team
-def send_email_to_recluta(prospect):
-    try:
-        global last_email_index
-
-        # Get the next email recipient
-        to_email = RECIPIENTS[last_email_index]
-
-        # Create context with prospect data
-        context = {'prospect': prospect}
-
-        # Render the email template with the context
-        email_content = render_to_string('recluta_email_template.html', context)
-
-        # Create and send the email message
-        email = EmailMessage(
-            subject='¡Nuevo prospecto registrado!',
-            body=email_content,  # Pass the HTML content directly to the body
-            from_email=settings.EMAIL_HOST_USER,
-            to=[to_email],
-        )
-        email.content_subtype = 'html'  # Set content type as HTML
-        email.fail_silently = False
-
-        # Send email to Recluta Team
-        email.send()
-        print(f'Email sent to {to_email}!')
-
-        # Update the index for the next email
-        last_email_index = (last_email_index + 1) % len(RECIPIENTS)
-    except Exception as e:
-        print(f'Error sending email: {str(e)}')
-
 # Create a new prospect
 def create_prospect(request):
     if request.method == 'POST':
@@ -83,9 +50,71 @@ def create_prospect(request):
             # Send email to Recluta Team
             send_email_to_recluta(prospect)
 
+            # Send email to prospect
+            send_thank_you_email(prospect.email)
+
             return redirect('/prospects/')
         except Exception as e:
             print(f'Error creating prospect: {str(e)}')
             return redirect('/')
     else:
         return redirect('/')
+
+# Send email to recluta team
+def send_email_to_recluta(prospect):
+    try:
+        global last_email_index
+
+        # Get the next email recipient
+        to_email = RECIPIENTS[last_email_index]
+
+        # Create context with prospect data
+        context = {'prospect': prospect}
+
+        # Render the email template with the context
+        email_content = render_to_string('email/recluta.html', context)
+
+        # Create and send the email message
+        email = EmailMessage(
+            subject='¡Nuevo prospecto registrado!',
+            body=email_content,  # Pass the HTML content directly to the body
+            from_email=settings.EMAIL_HOST_USER,
+            to=[to_email],
+        )
+        email.content_subtype = 'html'  # Set content type as HTML
+        email.fail_silently = False
+
+        # Send email to Recluta Team
+        email.send()
+        print(f'Email sent to {to_email}!')
+
+        # Update the index for the next email
+        last_email_index = (last_email_index + 1) % len(RECIPIENTS)
+    except Exception as e:
+        print(f'Error sending email to Recluta Team: {str(e)}')
+
+# Send thank you email to prospect
+def send_thank_you_email(prospect_email):
+    try:
+        # Create context for the email
+        context = {'prospect_email': prospect_email}
+        print(context)
+
+        # Render the email template with the context
+        email_content = render_to_string('email/thanks.html', context)
+
+        # Create and send the email message
+        email = EmailMessage(
+            subject='¡Gracias por registrarte!',
+            body=email_content,  # Pass the HTML content directly to the body
+            from_email=settings.EMAIL_HOST_USER,
+            to=[prospect_email],  # Send the email to the prospect's email address
+        )
+        email.content_subtype = 'html'  # Set content type as HTML
+        email.fail_silently = False
+
+        # Send the email
+        email.send()
+        print(f'Thank you email sent to {prospect_email}!')
+    except Exception as e:
+        print(f'Error sending thank you email: {str(e)}')
